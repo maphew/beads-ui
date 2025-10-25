40d23f5cf27d38b7e746407dc99fd9ab2ad56546
matt wilkie <maphew@gmail.com>
2025-10-25 14:26:33 -0700

    Meet Effective Go and repo guidelines for exported symbols.

--- assets/beady/embed.go
+++ assets/beady/embed.go
@@ -2,5 +2,7 @@ package beady
 
 import "embed"
 
+// FS embeds Beady’s HTML templates and static assets (CSS/JS) for runtime use.
+//
 //go:embed templates/*.html static/*.css static/*.js
 var FS embed.FS
